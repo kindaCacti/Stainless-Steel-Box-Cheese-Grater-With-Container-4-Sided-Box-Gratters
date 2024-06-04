716b58f5b620919cab19d9bab2f66ff299a6fcb3
Rys00 <mateusz.ogniewski@gmail.com>
2024-06-05 01:47:39 +0200

    quality commmit

--- CheesyApp/app.cpp
+++ CheesyApp/app.cpp
@@ -86,43 +86,56 @@ void App::draw() {
 
 void App::mouseButtonCallback(GLFWwindow *window, int button, int action,
                               int mods) {
-  if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
-    int x = mousePos.x / 100 * 100;
-    int y = (800 - mousePos.y) / 100 * 100;
-    if (currentElement == nullptr) {
-      for (auto element : pieceElements) {
-        if (element->getX() == x && element->getY() == y) {
-          currentElement = element;
-          highlightElements[0]->setPos(x, y);
-          if (api.getBoard().at(x / 100, y / 100).color == api.turnOf()) {
-            api.choosePiece((x / 100) + 'a', (y / 100) + '1');
-            api.loadPossibleMoves();
-            auto moves = api.getPossibleMoves();
-            for (int i = 0; i < moves.size(); ++i) {
-              auto &m = moves[i];
-              highlightElements[i + 1]->setPos(x + m.delta_x * 100,
-                                               y + m.delta_y * 100);
-            }
-          }
+  if (button != GLFW_MOUSE_BUTTON_LEFT || action != GLFW_RELEASE)
+    return;
+
+  int x = mousePos.x / 100 * 100;
+  int y = (800 - mousePos.y) / 100 * 100;
+
+  if (currentElement != nullptr) {
+    auto move = getMove(currentElement->getX() / 100,
+                        currentElement->getY() / 100, x / 100, y / 100);
+    bool result = api.movePiece(move.substr(0, 2), move.substr(2, 2));
+    if (result) {
+      for (int i = 0; i < pieceElements.size(); ++i) {
+        if (pieceElements[i]->getX() == x && pieceElements[i]->getY() == y) {
+          delete pieceElements[i];
+          pieceElements.erase(pieceElements.begin() + i);
         }
       }
-    } else {
-      auto move = getMove(currentElement->getX() / 100,
-                          currentElement->getY() / 100, x / 100, y / 100);
-      bool result = api.movePiece(move.substr(0, 2), move.substr(2, 2));
-      if (result) {
-        for (int i = 0; i < pieceElements.size(); ++i) {
-          if (pieceElements[i]->getX() == x && pieceElements[i]->getY() == y) {
-            delete pieceElements[i];
-            pieceElements.erase(pieceElements.begin() + i);
-          }
+      currentElement->setPos(x, y);
+      bool promotion = api.getBoard().at(x / 100, 7 - y / 100).moves_done == -1;
+      if (promotion) {
+        std::string color(api.getBoard().at(x / 100, 7 - y / 100).color ==
+                                  PIECE_COLOR::WHITE
+                              ? "white"
+                              : "black");
+        std::string path = std::string("res/textures/") + color;
+        *currentElement = new AppElement<ImageRect>(
+            new ImageRect(path + std::string("/Queen.png"), 100, 100), false,
+            true, marginLeft + x, y);
+      }
+    }
+    for (auto element : highlightElements) {
+      element->setPos(1000, 1000);
+    }
+    currentElement = nullptr;
+  }
+
+  for (auto element : pieceElements) {
+    if (element->getX() == x && element->getY() == y) {
+      currentElement = element;
+      if (api.getBoard().at(x / 100, 7 - y / 100).color == api.turnOf()) {
+        highlightElements[0]->setPos(x, y);
+        api.choosePiece((x / 100) + 'a', (y / 100) + '1');
+        api.loadPossibleMoves();
+        auto moves = api.getPossibleMoves();
+        for (int i = 0; i < moves.size(); ++i) {
+          auto &m = moves[i];
+          highlightElements[i + 1]->setPos(x + m.delta_x * 100,
+                                           y - m.delta_y * 100);
         }
-        currentElement->setPos(x, y);
       }
-      for (auto element : highlightElements) {
-        element->setPos(1000, 1000);
-      }
-      currentElement = nullptr;
     }
   }
 }
--- CheesyApp/appElement.h
+++ CheesyApp/appElement.h
@@ -12,6 +12,10 @@ public:
       : mTickable(tickable), mDrawable(drawable), mX(x), mY(y) {}
   virtual ~AppElementInterface() {}
 
+  virtual AppElementInterface &operator=(AppElementInterface *other) {
+    return *this;
+  };
+
   virtual void setX(int x) {}
   virtual void setY(int y) {}
   virtual int getX() {}
@@ -30,6 +34,12 @@ public:
   AppElement(T *element, bool tickable, bool drawable, int x = 0, int y = 0)
       : mElement(element), AppElementInterface(tickable, drawable, x, y) {}
   ~AppElement() { delete mElement; }
+
+  AppElement &operator=(AppElement *other) {
+    delete mElement;
+    this = other;
+    return *this;
+  }
 
   void setX(int x) { mX = x; }
   void setY(int y) { mY = y; }
