a0e0b48fee61b11140d5c5f7f78a7905226fcf78
Rys00 <mateusz.ogniewski@gmail.com>
2024-06-05 16:38:37 +0200

    removed some warnings

--- CheesyApp/appElement.h
+++ CheesyApp/appElement.h
@@ -12,11 +12,14 @@ public:
       : mTickable(tickable), mDrawable(drawable), mX(x), mY(y) {}
   virtual ~AppElementInterface() {}
 
-  virtual void setX(int x) {}
-  virtual void setY(int y) {}
-  virtual int getX() {}
-  virtual int getY() {}
-  virtual void setPos(int x, int y) {}
+  void setX(int x) { mX = x; }
+  void setY(int y) { mY = y; }
+  int getX() { return mX; }
+  int getY() { return mY; }
+  void setPos(int x, int y) {
+    mX = x;
+    mY = y;
+  }
 
   virtual void tick(double delta) {}
   virtual void draw() {}
@@ -30,15 +33,6 @@ public:
   AppElement(T *element, bool tickable, bool drawable, int x = 0, int y = 0)
       : mElement(element), AppElementInterface(tickable, drawable, x, y) {}
   ~AppElement() { delete mElement; }
-
-  void setX(int x) { mX = x; }
-  void setY(int y) { mY = y; }
-  int getX() { return mX; }
-  int getY() { return mY; }
-  void setPos(int x, int y) {
-    mX = x;
-    mY = y;
-  }
 
   void tick(double delta) {
     if (mTickable)
